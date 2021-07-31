93afe808f2296555d740b252117f38baa3403375
Jake Suddreth <58892456+jsuddret@users.noreply.github.com>
2021-07-30 17:03:06 -0700

    Add files via upload

--- SudokuAlgorithm.cpp
+++ SudokuAlgorithm.cpp
@@ -11,30 +11,14 @@ using namespace std;
 string file = "Sudoku.txt";
 int num_rows = 9;
 
-void concatenate(vector<int> &v1,vector<int> &v2){
+void concatenate(vector<int> &v1,vector<int> v2){
 	v1.insert(v1.end(), v2.begin(), v2.end());
 }
 
-void write_grid(vector<vector<int>> &v, string f) {
-	string filename(file);
-	vector<string> lines;
-	string line;
-	ifstream input_file(filename);
-	getline(input_file, line);
-	// read file line by line and push to vector
-	while (getline(input_file, line)) {
-			lines.push_back(line);
-	}
-	input_file.close();
-	// add values into 2D vector
-	for (int i = 0; i < num_rows; i++) {
-		for (int j = 0; j < num_rows; j++) {
-			if (lines[i][1+3*j] != ' ') {
-				// convert ASCII to integer
-				v[i][j] = int(lines[i][1+3*j])-48;
-			}
-		}
-	}
+int div_three(int x) {
+	int temp;
+	x % 3 == 0 ? temp = x : temp = abs(x % 3 - x);
+	return temp;
 }
 
 vector<int> erase_duplicates(vector<int> &v) {
@@ -51,6 +35,35 @@ int gen(int i) {
 	// return random number, limited by a number, i
 	srand(time(0));
 	return rand() % i;
+}
+
+vector<int> get_block(vector<vector<int>> v, int i, int j) {
+	vector<int> b;
+	for (int k = div_three(i); k < div_three(i+3); k++) {
+		for (int l = div_three(j); l < div_three(j+3); l++) {
+			b.push_back(v[i][j]);
+			cout << v[i][j] << endl;
+		}
+	}
+	return b;
+}
+
+// extract column by index
+vector<int> get_column(vector<vector<int>> v, int i) {
+	vector<int> c;
+	for (int j = 0; j < num_rows; j++) {
+		c.push_back(v[j][i]);
+	}
+	return c;
+}
+	
+//extract row by index
+vector<int> get_row(vector<vector<int>> v, int i) {
+	vector<int> r;
+	for (int j = 0; j < num_rows; j++) {
+		r.push_back(v[i][j]);
+	}	
+	return r;
 }
 
 void print_2d_vector(vector<vector<int>> v) {
@@ -92,46 +105,59 @@ bool prompt(bool p, vector<vector<int>> v) {
 	return false;
 }
 
-// extract column by index
-void get_column(vector<int> &c, vector<vector<int>> v, int i) {
-	for (int j = 0; j < num_rows; j++) {
-		c.push_back(v[j][i]);
-	}
-}
-	
-//extract row by index
-void get_row(vector<int> &r, vector<vector<int>> v, int i) {
-	for (int j = 0; j < num_rows; j++) {
-		r.push_back(v[i][j]);
-	}	
-}
-
 // sudoku algorithm
 int sudoku(vector<vector<int>> &v) {
-	vector<int> new_vector{0};
 	vector<int> def{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	vector<int> block;
 	vector<int> col;
 	vector<int> row;
 	for (int i = 0; i < num_rows; i++) {
-		row.clear();
-		get_row(row, v, i);
 		for (int j = 0; j < num_rows; j++) {
 			if (v[i][j] == 0) {
+				vector<int> temp{0};
+				block.clear();
+				row.clear();
 				col.clear();
-				get_column(col, v, j);
-				print_vector(col);
-				concatenate(new_vector, def);
-				concatenate(new_vector, col);
-				concatenate(new_vector, row);
-				vector<int> final_vector = erase_duplicates(new_vector);
-				new_vector.clear();
-				// v[i][j] = new_vector[gen(new_vector.size())];
-				v[i][j] = final_vector[0];
+				block = get_block(v, i, j);
+				print_vector(block);
+				row = get_row(v, i);
+				col = get_column(v, j);
+				concatenate(temp, def);
+				concatenate(temp, block);
+				concatenate(temp, col);
+				concatenate(temp, row);
+				temp = erase_duplicates(temp);
+				print_vector(temp);
+				if (i == 1 && j == 7) {
+					return 1;
+				}
+				v[i][j] = temp[0];
 			}
 		}
-		return 0;
 	}
 	return 0;
+}
+
+void write_grid(vector<vector<int>> &v, string f) {
+	string filename(file);
+	vector<string> lines;
+	string line;
+	ifstream input_file(filename);
+	getline(input_file, line);
+	// read file line by line and push to vector
+	while (getline(input_file, line)) {
+			lines.push_back(line);
+	}
+	input_file.close();
+	// add values into 2D vector
+	for (int i = 0; i < num_rows; i++) {
+		for (int j = 0; j < num_rows; j++) {
+			if (lines[i][1+3*j] != ' ') {
+				// convert ASCII to integer
+				v[i][j] = int(lines[i][1+3*j])-48;
+			}
+		}
+	}
 }
 
 int main() {
